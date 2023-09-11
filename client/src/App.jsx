cb21946eb470c84d2a3c7243ca24d9fb41399f9e
Richardy Tanure <richardy.tanure@hotmail.com>
2023-09-11 19:15:53 -0300

    created login and register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import { Routes, Route, Navigate } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
+import "bootstrap/dist/css/bootstrap.min.css"
 
 import Chat from "./pages/Chat"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 
+import NavBar from "./components/NavBar"
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Chat />}/>
-      <Route path="/login" element={<Login />}/>
-      <Route path="/register" element={<Register />}/>
-      <Route path="*" element={<Navigate to="/" />}/>
-    </Routes>
+    <>
+      <NavBar />
+      <Container className="">
+        <Routes>
+          <Route path="/" element={<Chat />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+          <Route path="*" element={<Navigate to="/" />}/>
+        </Routes>
+      </Container>
+    </>
+    
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,30 @@
+import { Alert, Button, Form, Row, Col, Stack } from "react-bootstrap"
+
 const Register = () => {
-  return ( <>Register</> );
+  return (
+    <>
+      <Form>
+        <Row style={{
+          height: "100vh",
+          justifyContent: "center",
+          paddingTop: "10%"
+        }}>
+          <Col xs={6}>
+            <Stack gap={3}>
+              <h2>Register</h2>
+              <Form.Control type="text" placeholder="name"/>
+              <Form.Control type="email" placeholder="email"/>
+              <Form.Control type="password" placeholder="password"/>
+              <Button variant="primary" type="submit">
+                Register
+              </Button>
+              <Alert variant="danger"><p>An error</p></Alert>
+            </Stack>
+          </Col>
+        </Row>
+      </Form>
+    </>
+  );
 }
  
 export default Register;
