40b0cdde43e18b634ab21400cd36705ad92aa50b
Richardy Tanure <richardy.tanure@hotmail.com>
2023-09-18 17:42:07 -0300

    Messages APIs

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const cors = require("cors")
 const mongoose = require("mongoose")
+
 const userRoute = require("./Routes/userRoute")
 const chatRoute = require("./Routes/chatRoute")
+const messageRoute = require("./Routes/messageRoute")
 
 const app = express()
 require("dotenv").config()
@@ -12,6 +14,7 @@ app.use(cors())
 
 app.use("/api/users", userRoute)
 app.use("/api/chats", chatRoute)
+app.use("/api/messages", messageRoute)
 
 
 app.get("/", (req, res) => {
