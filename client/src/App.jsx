fc1aca9b371a238d98f082ff761cecb2c4c8a5f4
Richardy Tanure <richardy.tanure@hotmail.com>
2023-09-18 19:19:14 -0300

    Fetch Chats

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,20 +10,23 @@ import Register from "./pages/Register"
 import NavBar from "./components/NavBar"
 import { useContext } from "react"
 import { AuthContext } from "./context/AuthContext"
+import { ChatContextProvider } from "./context/ChatContext"
 
 function App() {
   const { user } = useContext(AuthContext)
   return (
     <>
-      <NavBar />
-      <Container className="">
-        <Routes>
-          <Route path="/" element={user ? <Chat /> : <Login />}/>
-          <Route path="/register" element={user ? <Chat /> : <Register />}/>
-          <Route path="/login" element={user ? <Chat /> : <Login />}/>
-          <Route path="*" element={<Navigate to="/" />}/>
-        </Routes>
-      </Container>
+      <ChatContextProvider user = { user }>
+        <NavBar />
+        <Container className="">
+          <Routes>
+            <Route path="/" element={user ? <Chat /> : <Login />}/>
+            <Route path="/register" element={user ? <Chat /> : <Register />}/>
+            <Route path="/login" element={user ? <Chat /> : <Login />}/>
+            <Route path="*" element={<Navigate to="/" />}/>
+          </Routes>
+        </Container>
+      </ChatContextProvider>
     </>
     
   )
--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,5 +1,12 @@
+import { useContext } from "react";
+import { ChatContext } from "../context/ChatContext";
+
 const Chat = () => {
+  const { userChats, isUserChatsLoading, userChatsError } = useContext(ChatContext);
+
+  console.log("User Chats: ", userChats)
+
   return <>Chat</>;
-}
- 
+};
+
 export default Chat;
